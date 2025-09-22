ca0d311a25e817e245c9d62260c4702926d0794a
Rishi <rishimaheshwari040@gmail.com>
2025-09-22 10:20:44 +0530

    up

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,6 @@ const App = () => {
         <Sonner />
         <BrowserRouter>
           <Routes>
-            {/* Public Routes */}
             <Route path="/" element={<Index />} />
             <Route path="/blogs" element={<Blogs />} />
             <Route path="/properties" element={<Index />} />
